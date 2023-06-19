41f09f39fa567d53fa57f425891f75228aa4db9e
wackytai <tainahmonteiro.info@gmail.com>
2023-06-19 13:47:14 +0100

    first commit

---
+++ Makefile
@@ -0,0 +1,25 @@
+NAME = philo
+LIB = philo.h
+C_SOURCES = philo.c
+B_SOURCES = 
+CC = @cc
+CFLAGS = -Wall -Wextra -Werror -g
+
+all:	$(NAME)
+$(NAME):	$(C_SOURCES)
+			@$(CC) $(CFLAGS) $(^) -o $(NAME) -fsanitize=address
+
+bonus: $(B_SOURCES)
+			@$(CC) $(CFLAGS) $(^) -o $(NAME) -fsanitize=address
+
+clean:
+		@rm -f *.o
+
+fclean: clean
+		@rm -f $(NAME)
+
+re: fclean all
+
+reb: fclean bonus
+
+.PHONY: all bonus clean fclean re reb
---
+++ philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlemos-m <tlemos-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/19 13:31:18 by tlemos-m          #+#    #+#             */
+/*   Updated: 2023/06/19 13:42:54 by tlemos-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+/* philo.c */
+int	main(int argc, char **argv);
+
+#endif
